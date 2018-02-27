b4c8769fb36e90fd5bffc5338b6b536f5b53d799
Chaucee <hi@chaucee.com>
2018-02-27 11:53:55 -0700

    buger controllers setup

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,25 @@
 // Linked files
-var connection = require('./connection.js');
+var connection = require('../config/connection.js');
 
 // create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+var orm = {
+    selectAll: function(tableInput, colToSearch, valOfCol) {
+        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, res) {
+            if (err) throw err; 
+            console.log(result);
+        });
+    },
+    insertOne: function() {
+        var queryString = "INSERT INTO burgers";
+        connection.query(queryString);
+    },
+    updateOne: function() {
+        var queryString = "UPDATE burgers WHERE ?? = ?";
+        connection.query(queryString);
+    }
+}
 
 // selectAll() 
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,54 @@
 var express = require('express');
-var burgerJS = require('burger');
+var burger = require('../models/burger.js');
 
 // Create the router for the app, and export the router at the end of your file.
 
+var router = express.Router();
 
+router.get("/", function(req, res){
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index:, hbsObject");
+    });
+});
 
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.burger_name 
+    ], function(result) {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+    var condition = "id = " + req.params.id.substr(1);
+    console.log("condition", condition);
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(409).end();
+        } else {
+            res.status(200).end();
+        }
+});
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id.substr(1);
+    burger.deleteOne(condition, function(result) {
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+//Export file
+module.exports = router;
